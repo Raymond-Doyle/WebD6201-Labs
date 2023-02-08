a9c1e67ba774bd1187f1dba80595ea1050ef298f
Raymond-Doyle <raymond.doyle1@dcmail.ca>
2023-02-08 15:31:26 -0500

    New Update

--- Lab1/scripts/EventHandler.js
+++ Lab1/scripts/EventHandler.js
@@ -4,29 +4,7 @@ var displayEvent = document.getElementById("displayEvent");
 var userDisplay = document.getElementById("userDisplay");
 var passDisplay = document.getElementById("passDisplay");
 
-//Click Event
-welcomeLabel.addEventListener("click", function (){
-    console.log("Event: click");
-    displayEvent.textContent = "Event: click";
-} );
 
-//Double Click Event
-welcomeLabel.addEventListener("dblclick", function (){
-    console.log("Event: dblclick");
-    displayEvent.textContent = "Event: dblclick";
-} );
-
-//Mouse Over Events
-welcomeLabel.addEventListener("mouseover", function (){
-    console.log("Event: mouseover");
-    displayEvent.textContent = "Event: mouseover";
-});
-
-//Mouse Out Events
-welcomeLabel.addEventListener("mouseout", function (){
-    console.log("Event: mouseout");
-    displayEvent.textContent = "Event: mouseout";
-});
 
 function validateForm(){
     let x = document.forms["myForm"]["username"].value;
--- Lab1/scripts/main.js
+++ Lab1/scripts/main.js
@@ -21,52 +21,52 @@
         mainContent.before(mainParagraph)
     
     }
-function DisplayContactList() {
-    if (localStorage.length > 0) {
-        let contactList = document.getElementById("contactList") // Our contact list in the table of the contact-list page
 
-        let data = "" // Add data to this variable. Append deserialized data from localStorage to data
-        let keys = Object.keys(localStorage) // Return a String Array of keys
-
-        let index = 1 // Count number of keys
-
-        // for every key in the keys collection
-        for (const key of keys) {
-            let contactData = localStorage.getItem(key) // Get localStorage data value related to the key
-            let contact = new Contact()
-            
-            contact.deserialize(contactData)
-
-            // Inject repeatable row into the contactList
-            data += `<tr>
-                <th scope="row" class="text-center">${ index }</th>
-                <td class="text-center">${ contact.Name }</td>
-                <td class="text-center">${ contact.ContactNumber }</td>
-                <td class="text-center">${ contact.EmailAddress }</td>
-                <td class="text-center"></td>
-                <td class="text-center"></td>
-            </tr>
-            `
-
-            index++
-        }
-
-        contactList.innerHTML = data
-    }
+function LogInfo(email, number, comments, name){
+    console.log(email);
+    console.log(number);
+    console.log(comments);
+    console.log(name);
+    
 }
 
+function DisplayHomepage() {
+
+    document.body.style.backgroundImage = "url('https://www.cnet.com/a/img/resize/dfd7a35e25a073bca0471407d8c0addb00b4f7f3/hub/2022/02/23/dbc979d5-4fd1-4bc0-8fdf-c938403b134a/elden-ring-30.jpg?auto=webp&fit=crop&height=675&width=1200')"
+
+}
+
+
+function DisplayContacts() {
+    console.log("Contact Us Page")
+
+    let submitButton = document.getElementById("submitButton")
+    let email = document.getElementById("emailAddress")
+    let fullName = document.getElementById("fullName")
+    let comments = document.getElementById("comments")
+    let contactNumber = document.getElementById("contactNumber")
+
+
+    submitButton.addEventListener("click", function() {
+
+        console.log("Name: " + fullName)
+        console.log("Info")
+
+        //LogInfo(email, contactNumber, comments, fullName)
+    })
+}
     function Start(){
         console.log("App Started!")
     
         switch (document.title){
             case "Homepage - WEBD6201 Lab 1":
-                DisplayButton()
+                DisplayHomepage()
                 break
             case "Products - WEBD6201 Lab 1":
                 DisplayButton()
                 break
             case "Contact - WEBD6201 Lab 1":
-                DisplayContactList()
+                DisplayContacts()
                 break
         }
 
